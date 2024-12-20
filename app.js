62b31390b715df997b81572e8e03de5e7ead3a0e
syedHammad350 <ahmed22ali456@gmail.com>
2024-12-20 19:05:30 +0500

    i added questions

--- app.js
+++ app.js
@@ -1,8 +1,134 @@
+let quesarry=[
+    {
+        num:'1',
+        question:'2 * 2 = ?',
+        Options:{
+            a:'4',
+            b:'3',
+            c:'6',
+            d:'5',
+
+        },
+        answer:'4',
+    },
+
+    {
+        num:'2',
+        question:'FULL FORM OF PSL ?',
+        Options:{
+            a:'pakistion sasti league',
+            b:'pakistion supreame league',
+            c:'pakistion super league ',
+            d:'pakistion simple league',
+
+        },
+        answer:'pakistion super league ',
+    },
+    {
+        num:'3',
+        question:'HOW MANY BONES ARE THERE IN THE HUMAN BODY?',
+        Options:{
+            a:'204',
+            b:'206',
+            c:'203',
+            d:'202',
+
+        },
+        answer:'206 ',
+    },
+    {
+        num:'4',
+        question:'SUN RISES IN WHISVH DIRECTION',
+        Options:{
+            a:'EAST',
+            b:'NORTH',
+            c:'SOUTH',
+            d:'WASTE',
+
+        },
+        answer:'EAST',
+    },
+    {
+        num:'5',
+        question:'2 * 2 = ?',
+        Options:{
+            a:'4',
+            b:'3',
+            c:'6',
+            d:'5',
+
+        },
+        answer:'4',
+    },
+    {
+        num:'6',
+        question:'2 * 2 = ?',
+        Options:{
+            a:'4',
+            b:'3',
+            c:'6',
+            d:'5',
+
+        },
+        answer:'4',
+    },
+    {
+        num:'7',
+        question:'2 * 2 = ?',
+        Options:{
+            a:'4',
+            b:'3',
+            c:'6',
+            d:'5',
+
+        },
+        answer:'4',
+    },
+    {
+        num:'8',
+        question:'2 * 2 = ?',
+        Options:{
+            a:'4',
+            b:'3',
+            c:'6',
+            d:'5',
+
+        },
+        answer:'4',
+    },
+    {
+        num:'9',
+        question:'2 * 2 = ?',
+        Options:{
+            a:'4',
+            b:'3',
+            c:'6',
+            d:'5',
+
+        },
+        answer:'4',
+    },
+    {
+        num:'10',
+        question:'2 * 2 = ?',
+        Options:{
+            a:'4',
+            b:'3',
+            c:'6',
+            d:'5',
+
+        },
+        answer:'4',
+    },
+   
+]
+
+
 
 let h1_name=document.querySelector('.set_name')
 let h1_email=document.querySelector('.set_email')
 let h1_rollno=document.querySelector('.set_rollno')
-let h1_institute=document.querySelector('.set_institute')
+let h1_institute=document.querySelector ('.set_institute')
 let form=document.querySelector('.form_container')
 let main_div=document.querySelector('.main_div')
 let forminputs=document.querySelector('.forminputs')
@@ -14,11 +140,11 @@ let startquiz=()=>{
     let user_rollno= document.querySelector(".user_rollno").value
     let user_institute= document.querySelector(".user_institute").value
 
-    // if(!user_name || !user_email ||!user_rollno || !user_institute){
-    //     alert('plz ENTER YOUR INFORMATION')
-    //     return
+    if(!user_name || !user_email ||!user_rollno || !user_institute){
+        alert('plz ENTER YOUR INFORMATION')
+        return
 
-    // }
+    }
     
     main_div.style.display='block'
    forminputs.style.display = 'none'
